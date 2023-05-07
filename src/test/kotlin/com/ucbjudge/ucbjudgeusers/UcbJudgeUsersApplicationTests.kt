10af665f51c406bab2c2ce76af6f1672cc0db563
Chris Apaza <chrisalanapazaa@gmail.com>
2023-05-06 20:19:07 -0400

    Initial commit

---
+++ src/test/kotlin/com/ucbjudge/ucbjudgeusers/UcbJudgeUsersApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ucbjudge.ucbjudgeusers
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UcbJudgeUsersApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
