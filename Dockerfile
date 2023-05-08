979d75a6e9cb6b0a034d0a41c4b79adf74dfd631
Chris Apaza <chrisalanapazaa@gmail.com>
2023-05-08 01:05:12 -0400

    updated for enviromental variables

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM eclipse-temurin:11-jdk-alpine
+
+EXPOSE 8282
+
+VOLUME /tmp
+
+ENV CONFIG_SERVER_URI="CONFIG_SERVER_URI"
+ENV CONFIG_SERVER_USER="CONFIG_SERVER_USER"
+ENV CONFIG_SERVER_PASSWORD="CONFIG_SERVER_PASSWORD"
+
+ARG DEPENDENCY=target/dependency
+COPY ${DEPENDENCY}/BOOT-INF/lib /app/lib
+COPY ${DEPENDENCY}/META-INF /app/META-INF
+COPY ${DEPENDENCY}/BOOT-INF/classes /app
+
+ENTRYPOINT ["java","-cp","app:app/lib/*","ucb.judge.ujusers.UjUsersApplicationKt"]
---
+++ build-docker.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Author: Ignacio Illanes Bequer
+
+# package the application
+
+./mvnw clean package -DskipTests
+mkdir -p target/dependency && (cd target/dependency; jar -xf ../*.jar)
+
+# build the docker image
+
+echo "####################"
+echo "Building docker image"
+echo "####################"
+
+echo -n "Docker container name (ucb-judge-users): "
+read container_name
+
+if [ -z "$container_name" ]; then
+    container_name="ucb-judge-users"
+fi
+
+while [[ -z "$version_tag" ]]; do
+  echo -n "$container_name version tag: "
+  read version_tag
+
+  if [[ -z "$version_tag" ]]; then
+    echo "Version tag cannot be empty!"
+  fi
+done
+
+docker build -t $container_name:$version_tag .
+
+if [ $? -eq 0 ]; then
+  echo "Operation completed successfully!"
+else
+  echo "Operation failed."
+fi
